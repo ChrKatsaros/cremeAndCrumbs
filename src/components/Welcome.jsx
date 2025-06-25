f077f67d49a1d06a6944d006c8de5091e8d6737f
KatsaWins <chr.katsaros99@gmail.com>
2025-06-25 12:23:12 +0300

    Redesign About and update What We Offer subtitle

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,6 +6,7 @@ import image1 from '../assets/image1.png';
 import rightdown from '../assets/right-down.png';
 import arrowdown from '../assets/arrow-down.png';
 import scribble from '../assets/scribble.png';
+import logo from '../assets/logo.png';
 
 function Welcome() {
   return (
@@ -24,12 +25,13 @@ function Welcome() {
       <img src={scribble} className="arrow arrow-mid-low-right" alt="arrow" />
       <div className="arrow-text arrow-text-mid-low-right">parisian soul</div>
       <div className="hero-image">
-        <img src={image2} alt="Pistachio Cake" />
+        <img src={image2} alt="" />
       </div>
 
       <div className="hero-text">
         <h1>Welcome to Crème & Crumbs</h1>
         <h3>Your Sweet Escape</h3>
+        <img src={logo} alt="logo" className="welcome-page-logo" />
         <p>
           <em>
             “At <strong>Crème & Crumbs</strong>, every pastry is a small moment
